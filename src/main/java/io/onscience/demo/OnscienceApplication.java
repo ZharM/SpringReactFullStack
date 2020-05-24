4561f1ec35127bb35699bd41dc9b6ac3f168bf50
Zhar.M <mehdizhar344@gmail.com>
2020-05-24 20:44:28 +0100

    Start Project

---
+++ src/main/java/io/onscience/demo/OnscienceApplication.java
@@ -0,0 +1,13 @@
+package io.onscience.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnscienceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnscienceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/onscience/demo/OnscienceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.onscience.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnscienceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
